a5f36959aead5980fb7a11fc900ca91093d3a0f0
ReyabSaluja <114021780+reyabs17@users.noreply.github.com>
2023-11-24 18:40:40 -0500

    Frontednipcwb

---
+++ hackproject/frontend/src/pages/SignUpPage.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+
+const SignUpPage = () => {
+	return (
+        <>
+            <div className="column1">
+                <Header />
+            </div>
+        </>
+	);
+};
+
+export default SignUpPage;
